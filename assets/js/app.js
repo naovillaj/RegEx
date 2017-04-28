568dc0094a4254af01c0e04c369cd8e9d01e870a
Naomi Villanueva <naovillaj@gmail.com>
2017-04-27 23:57:16 -0500

    validacion contraseña y fecha de nacimiento

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,11 @@
-
-var validaciones = document.getElementById("enviar");
 var password = document.getElementById("password");
 var cPassword = document.getElementById("cPassword");
 
+password.onchange = cPassword.onkeyup = passwordMatch;
 
-validaciones.addEventListener("click", function(e){
-  e.preventDefault();
-  if(password.value !== cPassword.value){
-    alert("Las contraseñas son distintas");
-    cPassword.focus();
-    console.log(password.value);
-  }
-
-})
+function passwordMatch() {
+    if(password.value !== cPassword.value)
+        cPassword.setCustomValidity('Las contraseñas no coinciden.');
+    else
+        cPassword.setCustomValidity('');
+}
