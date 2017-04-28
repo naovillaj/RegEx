9ad42d0a074c250b7a13781b15888389d4ad2b3f
Naomi Villanueva <naovillaj@gmail.com>
2017-04-27 22:51:23 -0500

    validar contraseña

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-var validaciones = document.getElementById("validaciones");
-var password = document.getElementById("password").value;
-var cPassword = document.getElementById("cPassword").value;
+var validaciones = document.getElementById("enviar");
+var password = document.getElementById("password");
+var cPassword = document.getElementById("cPassword");
 
 validaciones.addEventListener("click", function(e){
   e.preventDefault();
 
-  if(password !== cPassword){
+  if(password.value !== cPassword.value){
     alert("Las contraseñas son distintas");
     cPassword.focus();
     console.log(password.value);
